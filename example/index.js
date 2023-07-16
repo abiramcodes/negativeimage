39e2475fa3416cdab7e2457e14493ffd343d89c2
Lokesh Varman <30750553+LokeshVarman@users.noreply.github.com>
2023-07-16 18:10:35 +0530

    Add files via upload

---
+++ example/index.js
@@ -0,0 +1,8 @@
+const pencilSketch = require('negativeimage');
+
+// Specify the input and output paths
+const inputPath = 'flower.jpeg';
+const outputPath = 'black.jpg';
+
+// Call the pencilSketch function
+pencilSketch(inputPath, outputPath);
