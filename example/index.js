5939abb2509fb555ff7faa112b25d2bf0a83a64e
Lokesh Varman <30750553+LokeshVarman@users.noreply.github.com>
2023-07-23 20:50:37 +0530

    Update index.js
    Updated the code with posterize example

--- example/index.js
+++ example/index.js
@@ -3,6 +3,7 @@ const pencilSketch = require('negativeimage');
 // Specify the input and output paths
 const inputPath = 'flower.jpeg';
 const outputPath = 'black.jpg';
-
+const arg="";
 // Call the pencilSketch function
-pencilSketch(inputPath, outputPath);
+pencilSketch(inputPath, outputPath, "p");
+//pencilSketch(inputPath,outputPath)
