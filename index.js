b19155abe83fa2932b73511e687f45d6584d1b34
abiramakannan <codingkannan@gmail.com>
2023-07-23 19:18:20 +0530

    added posterize effect

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const Jimp = require('jimp');
 
-async function negativeimage(inputPath, outputPath) {
+async function negativeimage(inputPath, outputPath, posterize) {
   try {
     // Load the input image
     const image = await Jimp.read(inputPath);
+    
+    if(posterize === 'posterize'){
 
-    // Convert the image to grayscale
-    image.grayscale();
+      //apply posterize effect
+      image.posterize(4)
 
-    // Apply a Gaussian blur to the image
-    image.gaussian(3);
+      //apply brightness to the image
+      image.brightness(0.2)
 
-    // Invert the image colors
-    image.invert();
+      //apply contrast to enhance image
+      image.contrast(0.1)
+      
+    } else{
+      // Convert the image to grayscale
+      image.grayscale();
+  
+      // Apply a Gaussian blur to the image
+      image.gaussian(3);
+  
+      // Invert the image colors
+      image.invert();
+    }
 
     // Save the processed image
     await image.writeAsync(outputPath);
@@ -24,3 +37,8 @@ async function negativeimage(inputPath, outputPath) {
 }
 
 module.exports = negativeimage;
+
+const inputPath = 'example/flower.jpeg';
+const outputPath = './posterize.jpg';
+
+negativeimage(inputPath, outputPath,'posterize');
